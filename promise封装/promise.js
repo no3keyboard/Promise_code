c8b0781210618537234a107b0de906ec6383ae3e
zxx <1206294197@qq.com>
2022-10-30 21:58:25 +0800

    3

--- promise封装/promise.js
+++ promise封装/promise.js
@@ -6,19 +6,29 @@ function Promise(excutor){
     const _this = this;
     //函数声明，形参命名为data
     function resolve(data){
+        //判断状态
+        if(_this.PromiseState !== 'pending') return;
         //1.修改对象状态(promiseState)
         _this.PromiseState = 'fullfilled';
         //2.设置对象结果值(promiseResult)
         _this.PromiseResult = data;
     }
     function reject(data){
+        //判断状态
+        if(_this.PromiseState !== 'pending') return;
         //1.修改对象状态(promiseState)
         _this.PromiseState = 'rejected';
         //2.设置对象结果值(promiseResult)
         _this.PromiseResult = data;
     }
-    //同步调用执行器函数
-    excutor(resolve,reject);
+    try {
+        //同步调用执行器函数
+        excutor(resolve,reject);
+    } catch (error) {
+        //修改promise对象状态为失败,即是调用reject并传参
+        reject(error);
+    }
+    
 }
 
 //添加then方法
