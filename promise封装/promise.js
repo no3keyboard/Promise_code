ab85bb1daf6f5978416c4ef542b9a8d5ce83a91f
zxx <1206294197@qq.com>
2022-10-31 16:46:48 +0800

    5

--- promise封装/promise.js
+++ promise封装/promise.js
@@ -4,7 +4,7 @@ function Promise(excutor){
     this.PromiseState = 'pending';
     this.PromiseResult = null;
     //声明属性
-    this.callback = {};
+    this.callbacks = [];
     //保存实例对象的this的值
     const _this = this;
     //函数声明，形参命名为data
@@ -16,9 +16,9 @@ function Promise(excutor){
         //2.设置对象结果值(promiseResult)
         _this.PromiseResult = data;
         //调用成功的回调函数
-        if(_this.callback.onResolved){
-            _this.callback.onResolved(data);
-        }
+        _this.callbacks.forEach(item => {
+            item.onResolved(data);
+        })
     }
     function reject(data){
         //判断状态
@@ -28,9 +28,9 @@ function Promise(excutor){
         //2.设置对象结果值(promiseResult)
         _this.PromiseResult = data;
         //调用失败的函数回调
-        if(_this.callback.onRejected){
-            _this.callback.onRejected(data);
-        }
+        _this.callbacks.forEach(item => {
+            item.onRejected(data);
+        })
     }
     try {
         //同步调用执行器函数
@@ -39,7 +39,6 @@ function Promise(excutor){
         //修改promise对象状态为失败,即是调用reject并传参
         reject(error);
     }
-    
 }
 
 //添加then方法
@@ -54,9 +53,9 @@ Promise.prototype.then = function(onResolved,onRejected){
     //判断pending状态
     if(this.PromiseState === 'pending'){
         //保存回调函数
-        this.callback = {
+        this.callbacks.push({
             onResolved:onResolved,
             onRejected:onRejected
-        }
+        });
     }
 }
